1624b8eef77046e2a6e6bb82f264e9355d26cc10
Matt Leuer <mattleuer@gmail.com>
2019-05-19 15:38:00 -0700

    Added Post and Put request functionality.

--- server/api.js
+++ server/api.js
@@ -17,11 +17,40 @@ apiRouter.get('/minions', (req, res, next) => {
     next();
 });
 
+apiRouter.get('/minions/:minionId', (req, res, next) => {
+    const minionID = req.params.minionId;
+    const minion = getFromDatabaseById(minionType, minionID);
+    if (minion) {
+        res.send(minion)
+    }else {
+        res.status(404).send();
+    }
+});
+
+apiRouter.put('/minions/:minionId', (req, res, next) => {
+    const updatedMinion = req.body;
+    const minionUpdatedInDatabase = updateInstanceInDatabase(minionType, updatedMinion);
+    if (minionUpdatedInDatabase) {
+        res.send(minionUpdatedInDatabase);
+    }else {
+        res.status(404).send();
+    }
+
+});
+
 apiRouter.post('/minions', (req, res, next) => {
     const newMinion = req.body;
     const minionAddedToDatabase = addToDatabase(minionType, newMinion);
     if (minionAddedToDatabase) {
         res.status(201).send(minionAddedToDatabase);
+    }
+});
+
+apiRouter.delete('/minions/:minionId', (req, res, next) => {
+    const minionID = req.params.minionId;
+    const successfullyDeletedMinion = deleteFromDatabasebyId(minionType, minionID);
+    if (!successfullyDeletedMinion) {
+        res.status(404).send();
     }
 });
 
